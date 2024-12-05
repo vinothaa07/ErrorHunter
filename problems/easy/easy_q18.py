a9d7d4ba4044c434a3f603a68085b3e24979f5c3
mathi27 <mathiyuvarajan.dev@gmail.com>
2024-12-06 00:43:15 +0530

    Easy Prob fixes

---
+++ problems/easy/easy_q18.py
@@ -0,0 +1,6 @@
+# Print Last Character of String
+def last_char_of_string(s):
+    return s[-2]  # Bug: Fetches second-to-last character instead of last
+if __name__ == "__main__":
+ # Handle the input  by Yourself
+ last_char_of_string()
