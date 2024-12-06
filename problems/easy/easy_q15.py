fd9fe9c4f343c673ce240de98392c5ad0d68a5c4
BoomikaSN <boomikasn2006@gmail.com>
2024-12-06 15:02:03 +0530

    debugged the errors (changed the conditions) in vowel or constant que14

--- problems/easy/easy_q15.py
+++ problems/easy/easy_q15.py
@@ -1,14 +1,2 @@
-# Grade Description: Write a program that accepts a grade (A, B, C, D, F) and prints its description (e.g., A = Excellent, B = Good, etc.) using a switch case.
-def grade_description(grade):
-    switch = {
-        'A': "Good",
-        'B': "Average",
-        'C': "Poor",
-        'D': "Excellent",  
-        'F': "Fail"
-    }
-    return switch.get(grade, "Not a valid grade") 
-if __name__ == "__main__":
-    rs = grade_description('Z')
-    print(rs)
+
     
