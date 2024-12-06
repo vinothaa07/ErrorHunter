1e4c26565fa91b5d60e2a0d20f4927a5c60f6321
Jeffrey Shalom <jeffshalom06@gmail.com>
2024-12-06 12:44:11 +0530

    Modified by Jeffrey

--- problems/easy/easy_q10.py
+++ problems/easy/easy_q10.py
@@ -2,14 +2,14 @@
 def is_palindrome(num):
     original = num
     reverse = 0
-    do:
+    while num != 0:
         reverse = reverse * 10 + num % 10
         num //= 10
-    while num != 0 
+    
     return reverse == original
+
 if __name__ == "__main__":
     nums = int(input("Enter the Number: "))
     res = is_palindrome(nums)
     print(res)
-    
     
