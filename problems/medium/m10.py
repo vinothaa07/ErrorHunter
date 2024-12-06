72eeb8898ab17eac32e5ae7ec479a1aad54f9a75
Heamanth007 <heamanth007@gmail.com>
2024-12-06 15:10:01 +0530

    The program for a gaming menu's  issue was identified and resolved

--- problems/medium/m10.py
+++ problems/medium/m10.py
@@ -10,24 +10,38 @@ def game_menu():
 
         if choice == 1:
             target = random.randint(1, 100)
-            guess = int(input("Guess a number between 1 and 100: "))
-            if guess = target:   
-                print("You won!")
-            else:
-                print("Try Again")
+            while True:
+                guess = int(input("Guess a number between 1 and 100: "))
+                if guess == target:   
+                    print("You won!")
+                    break
+                elif guess>target:
+                    print("Your guess is High")
+                elif guess<target:
+                    print("Your guess is Low")
         elif choice == 2:
             user_choice = input("Enter Rock, Paper, or Scissors: ").lower()
             options = ["rock", "paper", "scissors"]
-            computer_choice = options[random.randint(0, 2)]
-            if user_choice == computer_choice:
-                print("You Lose!")   
-            else:
-                print("Computer Wins!")
+            if user_choice in options:
+                computer_choice = options[random.randint(0, 2)]
+                print(f"Computer choice: {computer_choice.capitalize()}")
+                if (user_choice == options[0] and computer_choice == options[2]) or (user_choice == options[1] and computer_choice == options[0]) or (user_choice == options[2] and computer_choice == options[1]):
+                    print("You WON!")   
+                else:
+                    print("Computer Wins!")
         elif choice == 3:
-            dice = random.randint(1, 8)  
-            print("Dice rolled:", dice)
+            while True:
+                dice = random.randint(1, 6)  
+                print("Dice rolled:", dice)
+                n=input("Want to roll again? (y/n): ")
+                if n.lower()=='n':
+                    break
+                else:
+                    continue
         elif choice == 4:
             print("Exiting...")
             break
         else:
             print("Invalid Choice")
+
+game_menu()
