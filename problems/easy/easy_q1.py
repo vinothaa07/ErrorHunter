d8985b5547e0b8859882097cc0f83457f189b72a
Anushri Thirumavalavan <anushri6688@gmail.com>
2024-12-06 09:00:53 +0200

    changed the logical error

--- problems/easy/easy_q1.py
+++ problems/easy/easy_q1.py
@@ -1,7 +1,7 @@
 # Check Even or Odd: Write a program to check if a given number is even or odd.
 
 num = int(input("Enter a number: "))
-if (num / 2) != 0:
-   print("{0} is Odd".format(num))
+if num % 2 != 0:
+   print("{0} is odd".format(num))
 else:
-   print("{0} is Even".format(num))
+   print("{0} is even".format(num))
