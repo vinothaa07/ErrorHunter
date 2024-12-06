5dffcf3595abbd944c476438c3642f6d518ed22b
sabarinathan150607 <sabarinathan3452@gmail.com>
2024-12-06 16:06:24 +0530

    reversing number debugged code

--- problems/easy/easy_q8.py
+++ problems/easy/easy_q8.py
@@ -3,9 +3,8 @@ def reverse_number(num):
     rev = 0
     while num != 0:
         digit = num % 10
-        rev = rev + digit   
-        num //= 10
-    return num   
+        print(digit)
+        num = num //10  
 if __name__ == "__main__":
     num = int(input("Enter num : "))
     res = reverse_number(num)
