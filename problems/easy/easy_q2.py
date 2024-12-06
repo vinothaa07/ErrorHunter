5cdf3cb3096648bb53254c8026f5ece465857ca6
Srihari-tech2dive <sriharicbe07@gmail.com>
2024-12-06 12:15:41 +0200

    Update easy_q2.py
    I have fixed error

--- problems/easy/easy_q2.py
+++ problems/easy/easy_q2.py
@@ -4,9 +4,7 @@ def largest_of_two(a, b):
  
         return a
  
-         return b
-  
-        return a   
+         
      else:
         return b
 if __name__ == "__main__":
@@ -16,18 +14,4 @@ if __name__ == "__main__":
  
     print(res)
  
-    print(res)
- 
- 
-        print(num1,"larger")
-    else:
-        return b
-        print(num2,"larger")
-    
-if __name__ == "__main__":
-    num1 = int(input("Enter the First Number :"))
-    num2 = int(input("Enter the Second  :"))
-    res = largest_of_two(num1,num2)
-    print(res)
- 
- 
+   
