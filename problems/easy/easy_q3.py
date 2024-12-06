d45569d31c764784cfc3c95ae69ebeb905df1151
Sudarshan-prog <epicvisionsan@gmail.com>
2024-12-06 14:30:26 +0530

    in line 4 and 5 return statements changed or fixed

--- problems/easy/easy_q3.py
+++ problems/easy/easy_q3.py
@@ -1,8 +1,8 @@
 # Leap Year or Not: Write a program to determine whether a given year is a leap year.
 def is_leap_year(year):
     if year % 4 == 0 and year % 100 != 0 or year % 400 == 0:   
-        return "Not a Leap Year"   
-    return "Leap Year"
+        return "Leap Year"   
+    return "Not a Leap Year"
 if __name__ == "__main__":
     
     num = int(input("Enter the number :"))
