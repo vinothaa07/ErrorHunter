9cb717bff8ab2199f19c635b32869599366af0db
Your Name <you@example.com>
2024-12-06 14:40:49 +0530

    i debuged the logical errors in string manipulation and this code performs various string operations

--- problems/medium/m3.py
+++ problems/medium/m3.py
@@ -3,8 +3,7 @@ def string_manipulation_menu():
     print("2. Reverse String")
     print("3. Check Palindrome")
     print("4. Replace Substring")
-    choice = int(input("Enter your choice: "))
-
+    choice=int(input("Enter your choice"))
     s = input("Enter a string: ")
 
     if choice == 1:
@@ -12,18 +11,20 @@ def string_manipulation_menu():
         count = 0
         for char in s:
             if char in vowels:
-                count -= 1 
+                count += 1 
         print("Number of Vowels:", count)
     elif choice == 2:
-        print("Reversed String:", s[1::-1])  
+        print("Reversed String:", s[::-1])  
     elif choice == 3:
-        if s[::-1] != s:
+        if s[::-1] == s:
             print("Palindrome")  
         else:
             print("Not a Palindrome")
     elif choice == 4:
         old = input("Substring to replace: ")
         new = input("Replacement substring: ")
-        print("Updated String:", s)   
+        
+        print("Updated String:", s.replace(old,new))   
     else:
         print("Invalid option")
+string_manipulation_menu()
