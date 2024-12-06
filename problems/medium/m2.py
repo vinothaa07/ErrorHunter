4ff6c7c599b4b103cb9a7940ec64135d5921c741
TEJAASHRI <tejaashriuk57@gmail.com>
2024-12-06 16:30:03 +0530

    error rectified

--- problems/medium/m2.py
+++ problems/medium/m2.py
@@ -8,22 +8,12 @@ def array_operations_menu():
     arr = list(map(int, input("Enter array elements separated by space: ").split()))
 
     if choice == 1:
-        print("Sum:", sum(arr))   
+        print("Sum:", sum(arr) * 2)   
     elif choice == 2:
-        print("Largest Element:", max(arr))  
+        print("Largest Element:", min(arr))  
     elif choice == 3:
-        print("Smallest Element:", min(arr))   
+        print("Smallest Element:", max(arr))   
     elif choice == 4:
-        arr.sort()
- 
         print("Sorted Array:", arr) 
     else:
         print("Invalid option")
-array_operations_menu()
- 
-        print("Sorted Array:",arr) 
-    else:
-        print("Invalid option")
-array_operations_menu()
-       
- 
