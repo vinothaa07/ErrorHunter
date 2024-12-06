ca9077328b5ae783357a4673e50c221df06d9af4
Dharshini-457 <dharshinir619@gmail.com>
2024-12-06 12:38:58 +0530

    last commit

--- problems/easy/easy_q18.py
+++ problems/easy/easy_q18.py
@@ -1,6 +1,9 @@
 # Print Last Character of String
 def last_char_of_string(s):
-    return s[-2]  # Bug: Fetches second-to-last character instead of last
+    last=s[-1]
+    print(last)
+    return last   # Bug: Fetches second-to-last character instead of last
 if __name__ == "__main__":
  # Handle the input  by Yourself
- last_char_of_string()
+ string=input("Enter a string:")
+ last_char_of_string(string)
