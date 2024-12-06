c3670250932212e9456383632feed9dfb6c288fe
sanjaysabarinathan <sanjayboopathi07@gmail.com>
2024-12-06 12:48:42 +0530

    error solved in index and print fun

--- problems/easy/easy_q18.py
+++ problems/easy/easy_q18.py
@@ -1,6 +1,8 @@
 # Print Last Character of String
 def last_char_of_string(s):
-    return s[-2]  # Bug: Fetches second-to-last character instead of last
+    return s[-1]  # Bug: Fetches second-to-last character instead of last
 if __name__ == "__main__":
  # Handle the input  by Yourself
- last_char_of_string()
+ str=input("Enter the string :")
+last_str= last_char_of_string(str)
+print(last_str)
