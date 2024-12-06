3638007d3987203968c19bcefad3e33a9d373c2c
SANJAY <sanjayjeya2006@gmail.com>
2024-12-06 13:51:38 +0530

    added f format which was not added before in checking even or odd

--- problems/easy/easy_q1.py
+++ problems/easy/easy_q1.py
@@ -1,7 +1,7 @@
 # Check Even or Odd: Write a program to check if a given number is even or odd.
 
 num = int(input("Enter a number: "))
-if (num / 2) != 0:
-   print(f"{0} is Odd{(num)}")
+if (num%2) != 0:
+   print(f"{num} is Odd")
 else:
-   print(f"{0} is Even{(num)}")
+   print(f"{num} is Even")
