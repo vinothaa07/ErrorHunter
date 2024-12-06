05c5a0664915e0d73182b66d76a3b95d6f35a59a
mourishantony <mourishantonyc@gmail.com>
2024-12-06 14:13:45 +0530

    here there is an error in increment operater

--- problems/easy/easy_q6.py
+++ problems/easy/easy_q6.py
@@ -3,9 +3,8 @@ def print_numbers(n):
     i = 1
     while i <= n:
         print(i)
-        n -= 1   
+        i += 1   
 
 if __name__ == "__main__":
     num = int(input("Enter the Number "))
-    res = print_numbers(num)
-    print(res)
+    print_numbers(num)
