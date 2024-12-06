d029d2fbc4b191382fd1ecc827d6aea65c9c45db
Sivaram1457 <sivaram1457@gmail.com>
2024-12-06 14:15:44 +0530

    changed logic error in code of rock paper scissors

--- problems/easy/easy_q1.py
+++ problems/easy/easy_q1.py
@@ -4,4 +4,4 @@ num = int(input("Enter a number: "))
 if (num / 2) != 0:
    print("{0} is Odd".format(num))
 else:
-   print("{0} is Even".format(num))
+   print(f"{num} is Even")
--- problems/medium/m10.py
+++ problems/medium/m10.py
@@ -9,9 +9,9 @@ def game_menu():
         choice = int(input("Enter your choice: "))
 
         if choice == 1:
-            target = random.randint(1, 100)
+            target = random.randint(1, 101)
             guess = int(input("Guess a number between 1 and 100: "))
-            if guess = target:   
+            if guess == target:   
                 print("You won!")
             else:
                 print("Try Again")
@@ -19,15 +19,30 @@ def game_menu():
             user_choice = input("Enter Rock, Paper, or Scissors: ").lower()
             options = ["rock", "paper", "scissors"]
             computer_choice = options[random.randint(0, 2)]
+            print(f"computer choice is {computer_choice}")
             if user_choice == computer_choice:
-                print("You Lose!")   
-            else:
-                print("Computer Wins!")
+                print("Draw")   
+            elif user_choice == "scissors":
+                if computer_choice == "paper":
+                    print("You won")
+                else:
+                    print("you lost")
+            elif user_choice =="paper":
+                if computer_choice =="rock":
+                    print("you won")
+                else:
+                    print("you lost")
+            elif user_choice =="rock":
+                if computer_choice =="scissors":
+                    print("you won")
+                else:
+                    print("you lost")
         elif choice == 3:
-            dice = random.randint(1, 8)  
+            dice = random.randint(1, 7)  
             print("Dice rolled:", dice)
         elif choice == 4:
             print("Exiting...")
             break
         else:
             print("Invalid Choice")
+game_menu()
