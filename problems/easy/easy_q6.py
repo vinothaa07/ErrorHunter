db9ec1a64b6da709e2a18efb3c463560c9dd88e3
samikshaviji <samikshavijay15@gmail.com>
2024-12-06 09:22:09 +0200

    changes made in the codes from 1 to 5

--- problems/easy/easy_q6.py
+++ problems/easy/easy_q6.py
@@ -1,11 +1,10 @@
 # Print Numbers from 1 to N: Write a program to print numbers from 1 to a given number N using a while loop.
-def print_numbers(n):
-    i = 1
-    while i <= n:
-        print(i)
-        n -= 1   
+i = 0
+while i <= n:
+    print(i)
+    n += 1   
 
 if __name__ == "__main__":
-    num = int(input("Enter the Number "))
+    num = int(input("Enter the Number:"))
     res = print_numbers(num)
     print(res)
