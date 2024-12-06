241506876be12a4ce897f442e5aa178c785894ad
Vinu Karthick D <vinukarthick6@gmail.com>
2024-12-06 14:21:11 +0530

    fix: m1

--- problems/medium/m1.py
+++ problems/medium/m1.py
@@ -6,26 +6,26 @@ def math_operations_menu(choice):
     a, b = map(int, input("Enter two numbers(separated by commas): ").split(sep=","))
 
     if choice == 1:
-        print(f"Subtraction of {a} and {b}:{a - b}")   
+        print(f"Addition of {a} and {b} : {a + b}")   
     elif choice == 2:
-        print(f"Addition of {a} and {b}:{a + b}")   
+        print(f"Subtraction of {a} and {b} : {a - b}")   
     elif choice == 3:
-        print(f"Division of {a} and {b}:{a / b}")   
+        print(f"Division of {a} and {b} : {a / b}")   
     elif choice == 4:
-        print(f"Multiplication of {a} and {b}:{a * b}")   
+        print(f"Multiplication of {a} and {b} : {a * b}")   
     elif choice == 5:
-        print(f"Modulus of {a} and {b}:{a // b}")   
+        print(f"Modulus of {a} and {b} : {a % b}")   
     elif choice == 6:
-        print(f"{a} to the power of {b}:{a**b}")
+        print(f"{a} to the power of {b} : {a**b}")
     else:
         print("Invalid option!!!")
 print("-------------Mathematical operation menu---------------")
-print("1. Add")
-print("2. Subtract")
+print("1. Subtract")
+print("2. Add")
 print("3. Multiply")
 print("4. Divide")
 print("5. Modulus")
-print("6. Expontential")
+print("6. Exponential")
 choice = int(input("Enter your choice: "))
 math_operations_menu(choice)
 print("-------------------------------------------------------")
