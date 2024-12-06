3538694fd5c480ad45abfc60149f87feb575f13b
BoomikaSN <boomikasn2006@gmail.com>
2024-12-06 14:41:40 +0530

    changed the condition in print numbers from 1 to n

--- problems/easy/easy_q7.py
+++ problems/easy/easy_q7.py
@@ -10,3 +10,13 @@ if __name__ == "__main__":
     num = int(input("Enter the Number : "))
     
     
+def sum_of_digits(num):
+    total = 0
+    while num > 0:
+        total += num % 10  # Add the last digit to total
+        num //= 10  # Remove the last digit
+    return total
+
+if __name__ == "__main__":
+    num = int(input("Enter the Number: "))
+    print("The sum of the digits is:", sum_of_digits(num))
