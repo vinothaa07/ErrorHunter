92373fda68029a2f1e714a278cc44d3fbdb8d6ca
sanjaysabarinathan <sanjayboopathi07@gmail.com>
2024-12-06 12:29:44 +0530

    solved on issue in n times printing

--- problems/easy/easy_q17.py
+++ problems/easy/easy_q17.py
@@ -3,5 +3,6 @@ def print_x_n_times(x, n):
     for i in range(1, n):  # Bug: Loop runs one less time than expected
         print(x)
 if __name__ == "__main__":
- # Handle the input  by Yourself
- print_x_n_times()
+ x=input("Enter the character to print :")
+ n=int(input("Enter the number of times to print :"))
+ print_x_n_times(x,n)
