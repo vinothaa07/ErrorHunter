1924c16a6b9f0f0120a75571f074f89c5bb9dd30
Dharshini-457 <dharshinir619@gmail.com>
2024-12-06 14:18:47 +0530

    change the code using loop

--- problems/medium/m17.py
+++ problems/medium/m17.py
@@ -1,19 +1,12 @@
 '''Write a program to reverse a given list without using built-in functions'''
 def reverse_list(lst):
+    lists=[]
     start = -1
-    end = len(lst) 
-    
-    
-    while start > end:
-       
-        lst[start], lst[end] = lst[end], lst[start]
-        
-         
-        start -= 1
-        end += 1
-    
-    return lst
- 
+    end=len(lst)
+    for i in lst:
+        st=lst[-i]
+        lists.append(st)
+    return(lists)
 input_list = [1, 2, 3, 4, 5]
 print("Original list:", input_list)
 
