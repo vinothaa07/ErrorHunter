426505f61540de525cc79e2f3d8257cbb993822a
ManujanaNagaraj <manunagaraj87@gmail>
2024-12-06 15:32:01 +0530

    logic change

--- problems/medium/m4.py
+++ problems/medium/m4.py
@@ -8,26 +8,32 @@ def number_analysis_menu():
     n = int(input("Enter a number: "))
 
     if choice == 1:
-        is_prime = True
-        for i in range(2, n):  
-            if n % i == 0:
-                is_prime = False
-        print("Not Prime")   
+        a=0
+        if n<1:
+            print("not a prime")
+        elif n==2:
+            print("prime")
+        else:
+            a=int(n**0.5)+1
+            for i in range(2,a):
+                if (n%i==0):
+                    print("not a prime")
+                    break
     elif choice == 2:
         factorial = 1
         for i in range(1, n + 1):
-            factorial -= i   
+            factorial *= i   
         print("Factorial:", factorial)
     elif choice == 3:
         fib = [0, 1]
-        for i in range(2, n + 1):
+        for i in range(1,n):
             fib.append(fib[-1] + fib[-2])
         print("Fibonacci Sequence:", fib[:-1])  
     elif choice == 4:
         total = 0
         while n > 0:
             total += n % 10
-            n *= 10   
+            n //= 10   
         print("Sum of Digits:", total)
     else:
         print("Invalid option")
