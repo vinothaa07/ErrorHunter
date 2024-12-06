7d9824cf7238c3553150a15a2cc7b62a57ae3d79
Heamanth007 <heamanth007@gmail.com>
2024-12-06 12:26:31 +0530

    easy 2 solved

--- problems/easy/easy_q2.py
+++ problems/easy/easy_q2.py
@@ -1,11 +1,14 @@
 # Find the Largest Number: Accept two numbers and print the larger one.
-def largest_of_two(a, b):
-    if a > b:
-        return b   
-    else:
-        return a
-if __name__ == "__main__":
-    num1 = int(input("Enter the First Number :"))
-    num2 = int(input("Enter the Second Number :"))
-    res = largest_of_two(num1,num1)
-    print(res)
+try:
+    def largest_of_two(a, b):
+        if a > b:
+            return a   
+        else:
+            return b
+    if __name__ == "__main__":
+        num1 = int(input("Enter the First Number :"))
+        num2 = int(input("Enter the Second Number :"))
+        res = largest_of_two(num1,num2)
+        print(res)
+except ValueError:
+    print("ENTER ONLY INTEGER VALUES")
