e200d104c96fe75f63751c560f9827221c4cd9ea
Heamanth007 <heamanth007@gmail.com>
2024-12-06 12:12:51 +0530

    The issue of this code was resolved and error handling was included

--- problems/easy/easy_q1.py
+++ problems/easy/easy_q1.py
@@ -1,7 +1,9 @@
 # Check Even or Odd: Write a program to check if a given number is even or odd.
-
-num = int(input("Enter a number: "))
-if (num / 2) != 0:
-   print("{0} is Odd".format(num))
-else:
-   print("{0} is Even".format(num))
+try:
+   num = int(input("Enter a number: "))
+   if num % 2 != 0:
+      print(f"{num} is Odd")
+   else:
+      print(f"{num} is Even")
+except ValueError:
+   print("Enter only INTEGER that is ONLY NUMBER")
