15fa5482f86777217e9026953f57f0cd6a785fd7
kishoreJothish <kishorejothish7@gmail.com>
2024-12-06 15:22:25 +0200

    logical error changed

--- problems/medium/m1.py
+++ problems/medium/m1.py
@@ -10,17 +10,20 @@ def math_operations_menu():
     print("5. Modulo")
     choice = int(input("Enter your choice: "))
 
-    a, b = map(int, input("Enter two numbers: ").split())
-
-    if choice == 1:
+    a = int(input("Enter  number1: "))
+    b = 1
+    int(input("Enter  number2: "))
+    if choice == 2:
         print("Subtraction:", a - b)   
-    elif choice == 2:
+    elif choice == 1:
         print("Addition:", a + b)   
-    elif choice == 3:
-        print("Division:", a / b)   
     elif choice == 4:
+        print("Division:", a / b)   
+    elif choice == 3:
         print("Multiplication:", a * b)   
     elif choice == 5:
         print("Modulo:", a // b)   
     else:
         print("Invalid option")
+
+math_operations_menu()
