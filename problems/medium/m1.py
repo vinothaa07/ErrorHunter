9773f4dd954d1a97f4729a73a30c94136463a4f3
sanjaysabarinathan <sanjayboopathi07@gmail.com>
2024-12-06 15:23:03 +0530

    Error solved in symbols and  calling function

--- problems/medium/m1.py
+++ problems/medium/m1.py
@@ -10,17 +10,18 @@ def math_operations_menu():
     print("5. Modulo")
     choice = int(input("Enter your choice: "))
 
-    a, b = map(int, input("Enter two numbers: ").split())
+    a, b = map(int ,input("Enter two numbers: ").split())
 
     if choice == 1:
-        print("Subtraction:", a - b)   
-    elif choice == 2:
         print("Addition:", a + b)   
+    elif choice == 2:
+        print("Subration:", a - b)   
     elif choice == 3:
-        print("Division:", a / b)   
-    elif choice == 4:
         print("Multiplication:", a * b)   
+    elif choice == 4:
+        print("Division:", a / b)   
     elif choice == 5:
         print("Modulo:", a // b)   
     else:
         print("Invalid option")
+math_operations_menu()
