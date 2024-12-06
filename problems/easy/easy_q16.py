c4973a2ec71a396fcd4a20af8f7b115277c278e6
mourishantony <mourishantonyc@gmail.com>
2024-12-06 11:19:58 +0100

    I have corrected the code

--- problems/easy/easy_q16.py
+++ problems/easy/easy_q16.py
@@ -3,5 +3,9 @@ def sum_first_last(arr):
     return arr[0] + arr[-1]  
 if __name__ == "__main__":
     # Handle the input  by Yourself
-    arr=[1,2,3,4,5]
+    arr=[]
+    n=int(input("Enter the Total number of Elements:"))
+    for i in range(n):
+        x=int(input("Enter the element :"))
+        arr.append(x)
     print(sum_first_last(arr))
--- problems/easy/easy_q17.py
+++ problems/easy/easy_q17.py
@@ -1,7 +1,8 @@
 # Print X N Times
-def print_x_n_times(x, n):
-    for i in range(1, n):  # Bug: Loop runs one less time than expected
-        print(x)
+def print_x_n_times(x, n):  # Bug: Loop runs one less time than expected
+        print(x*n)
 if __name__ == "__main__":
  # Handle the input  by Yourself
- print_x_n_times()
+ n=int(input("Enter the number of times:"))
+ x="X"
+ print_x_n_times(x,n)
