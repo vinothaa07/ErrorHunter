c00adfff9ea055cee4ec43f136c0c1f2f0b2557f
samikshaviji <samikshavijay15@gmail.com>
2024-12-06 15:52:07 +0530

    changes in medium

--- problems/easy/easy_q8.py
+++ problems/easy/easy_q8.py
@@ -1,7 +1,9 @@
  # Reverse a Number: Accept a number and print its reverse using a while loop.
 def reverse_number(num):
     rev = 0
-    while num != 0:
+    revs=reversed(num)
+    for x in revs:
+        print(x)
         digit = num % 10
         rev+=digit   
         num //= 10
