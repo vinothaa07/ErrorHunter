ea654fa419ba1c48af496203d1758b9a5a6477b4
Your Name <you@example.com>
2024-12-06 12:29:41 +0530

    enhanced code

--- problems/easy/easy_q2.py
+++ problems/easy/easy_q2.py
@@ -8,4 +8,4 @@ if __name__ == "__main__":
     num1 = int(input("Enter the First Number :"))
     num2 = int(input("Enter the Second Number :"))
     res = largest_of_two(num1,num1)
-    print(res)
+    print("The largest number is:",res)
