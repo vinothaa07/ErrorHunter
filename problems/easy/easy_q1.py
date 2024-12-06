0c4658c31e8d37f0ed7af5d560c3f6d283112f93
Santhosh09062006 <sandy09062006@gmail.com>
2024-12-06 13:31:55 +0530

    elif satement is added to tell , 0 is neither odd nor even

--- problems/easy/easy_q1.py
+++ problems/easy/easy_q1.py
@@ -1,7 +1,9 @@
 # Check Even or Odd: Write a program to check if a given number is even or odd.
 
 num = int(input("Enter a number: "))
-if (num % 2) != 0:
+if num == 0:
+   print("0 is neither odd nor even")
+elif (num % 2) != 0:
    print("{0} is Odd".format(num))
 else:
    print("{0} is Even".format(num))
