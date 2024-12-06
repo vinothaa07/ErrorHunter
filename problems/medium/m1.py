480691bb5f500bc8993ba2e70bd60529a4e9634a
sabarinathan150607 <sabarinathan3452@gmail.com>
2024-12-06 14:56:18 +0530

    math operator code debugged

--- problems/medium/m1.py
+++ problems/medium/m1.py
@@ -10,17 +10,21 @@ def math_operations_menu():
     print("5. Modulo")
     choice = int(input("Enter your choice: "))
 
-    a, b = map(int, input("Enter two numbers: ").split())
-
-    if choice == 1:
+    a=int(input("enter a number1:"))
+    b=int(input("enter a number2:"))
+    if choice == 2:
         print("Subtraction:", a - b)   
-    elif choice == 2:
+    elif choice == 1:
         print("Addition:", a + b)   
-    elif choice == 3:
-        print("Division:", a / b)   
     elif choice == 4:
+        if b==0:
+            print("zero error")
+        else:
+            print("Division:", a / b)   
+    elif choice == 3:
         print("Multiplication:", a * b)   
     elif choice == 5:
         print("Modulo:", a // b)   
     else:
         print("Invalid option")
+math_operations_menu()
