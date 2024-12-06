7d81b9a1c99f35b43551999c8431f89afef276de
BoomikaSN <boomikasn2006@gmail.com>
2024-12-06 13:47:07 +0530

    changed the for loop condition  and added the input statement

--- problems/easy/easy_q16.py
+++ problems/easy/easy_q16.py
@@ -3,4 +3,5 @@ def sum_first_last(arr):
     return arr[1] + arr[-1]  
 if __name__ == "__main__":
     # Handle the input  by Yourself
+    arr=int()
     sum_first_last()
--- problems/easy/easy_q17.py
+++ problems/easy/easy_q17.py
@@ -1,7 +1,9 @@
 # Print X N Times
 def print_x_n_times(x, n):
-    for i in range(1, n):  # Bug: Loop runs one less time than expected
+    for i in range(n):  # Bug: Loop runs one less time than expected
         print(x)
 if __name__ == "__main__":
+    x=input("enter the string:")
+    n=int(input("enter number of times to diplay:"))
  # Handle the input  by Yourself
- print_x_n_times()
+print_x_n_times(x,n)
