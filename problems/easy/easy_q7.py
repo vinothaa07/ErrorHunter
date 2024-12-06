953d0fe27f70dc1aeb5d9385731446869bfd4768
Pavi022006 <pavithra.ratz@gmail.com>
2024-12-06 14:25:11 +0530

    debugged_7

--- problems/easy/easy_q7.py
+++ problems/easy/easy_q7.py
@@ -2,11 +2,14 @@
 def sum_of_digits(num):
     total = 0
     while num > 0:
-        total += num 
-        num +=1 
-    print(total)
+        digits = num%10
+        total += digits 
+        num //= 10
+    return total
 
 if __name__ == "__main__":
-    num = int(input("Enter the Number : "))
-    sum_of_digits(num)
+    number = int(input("Enter the Number : "))
+    print(sum_of_digits(number))
+    
+    
     
