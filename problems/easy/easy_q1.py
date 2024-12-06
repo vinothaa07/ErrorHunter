a2508d9368accf82a483d360f240e609fcd75906
ManujanaNagaraj <manunagaraj87@gmail>
2024-12-06 12:38:20 +0530

    changes

--- problems/easy/easy_q1.py
+++ problems/easy/easy_q1.py
@@ -2,6 +2,6 @@
 
 num = int(input("Enter a number: "))
 if (num / 2) != 0:
-   print("{0} is Odd".format(num))
+   print("{0} is even".format(num))
 else:
-   print("{0} is Even".format(num))
+   print("{0} is odd".format(num))
