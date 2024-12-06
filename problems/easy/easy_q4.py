51b8f84828f07e1f3f9d7dbf558fd5e856721679
mohamedimthiyas2007 <imthiyazsabiri1@gmail.com>
2024-12-06 13:36:29 +0000

    added f format whic 5th and 7th line'

--- problems/easy/easy_q4.py
+++ problems/easy/easy_q4.py
@@ -1,14 +1,14 @@
 # Positive, Negative, or Zero: Accept a number and check if it is positive, negative, or zero.
 def check_number(num):
     if num > 0:
-        print("Negative")  
+        print("positive")  
     elif num < 0:
-        print("Positive")  
+        print("negative")  
     else:
-        print("Number is negative")   
+        print("the number is zero")   
         
 if __name__ == "__main__":
-    num = input("Enter the Number : ")
+    num = int(input("Enter the Number : "))
     res = check_number(num)
     print(res)
     
