1927ade81612cb137c2897c0aa9d371346ffd0ba
mourishantony <mourishantonyc@gmail.com>
2024-12-06 13:58:25 +0530

    Here there is an mistake in data type input and also the in the if conditions

--- problems/easy/easy_q4.py
+++ problems/easy/easy_q4.py
@@ -1,16 +1,7 @@
 # Positive, Negative, or Zero: Accept a number and check if it is positive, negative, or zero.
 def check_number(num):
-    if num > 0:
-        print("Negative")  
-    elif num < 0:
-        print("Positive")  
-    else:
-        print("Number is negative")   
-        
+    print("The given number is Negative" if (num  < 0 ) else  "The given number is Positive" if (num > 0) else "The given number is Zero")
 if __name__ == "__main__":
-    num = input("Enter the Number : ")
-    res = check_number(num)
-    print(res)
-    
-    
+    num = int(input("Enter the Number : "))
+    check_number(num)
     
