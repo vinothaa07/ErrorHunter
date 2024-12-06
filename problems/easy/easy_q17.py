318881d863ffd142545e2f72093714395376dae1
Dharshini-457 <dharshinir619@gmail.com>
2024-12-06 13:46:03 +0530

    N times of x

--- problems/easy/easy_q17.py
+++ problems/easy/easy_q17.py
@@ -1,7 +1,8 @@
 # Print X N Times
-def print_x_n_times(x, n):
-    for i in range(1, n):  # Bug: Loop runs one less time than expected
-        print(x)
+def print_x_n_times(n):
+    for i in range(0, n):  # Bug: Loop runs one less time than expected
+        print("x")
 if __name__ == "__main__":
  # Handle the input  by Yourself
- print_x_n_times()
+ num=int (input(" Enter a N times :"))
+ print_x_n_times(num)
