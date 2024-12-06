a4fb0378d95b8b342d037c19251fd4aa8b0a3f71
shridhanya06 <shridhanyasb@outlook.com>
2024-12-06 15:02:20 +0530

    changes done from code 1 to 3

--- problems/easy/easy_q2.py
+++ problems/easy/easy_q2.py
@@ -7,5 +7,5 @@ def largest_of_two(a, b):
 if __name__ == "__main__":
     num1 = int(input("Enter the First Number :"))
     num2 = int(input("Enter the Second Number :"))
-    res = largest_of_two(num1,num1)
+    res = largest_of_two(num1,num2)
     print(res)
