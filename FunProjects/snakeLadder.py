81cc9c98237f63ef11c0e587c124da0d076b2819
Dharshini-457 <dharshinir619@gmail.com>
2024-12-07 13:56:47 +0530

    Snake and ladder game

--- FunProjects/snakeLadder.py
+++ FunProjects/snakeLadder.py
@@ -2,13 +2,18 @@ import random
 
 def snake_and_ladder():
     position = 0
-    while position != 100:
+    while (position < 100):
         dice = random.randint(1, 6)
         position += dice
-        if position == 50:  # Climbing a ladder
+        if position == 100:
+            print("You won the game!!")
+            break
+        elif position == 50:  # Climbing a ladder
             position = 75
         elif position == 25:  # Snake bite
             position = 10
         print(f"Dice: {dice}, Position: {position}")
+    else:
+        print("You  are in the 100 th place YOU won the game" )
 
 snake_and_ladder()
