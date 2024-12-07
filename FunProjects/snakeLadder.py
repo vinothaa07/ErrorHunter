593dd047f7b36cb0c7634dfd561600d2cf3ca4ae
Your Name <you@example.com>
2024-12-07 16:39:36 +0530

    updated  snake and ladder

--- FunProjects/snakeLadder.py
+++ FunProjects/snakeLadder.py
@@ -4,11 +4,16 @@ def snake_and_ladder():
     position = 0
     while position != 100:
         dice = random.randint(1, 6)
-        position += dice
-        if position == 50:  # Climbing a ladder
+        if position + dice <= 100:  
+            position += dice
+        if position == 50:  
             position = 75
-        elif position == 25:  # Snake bite
+        elif position == 25:  
             position = 10
+            
+           
         print(f"Dice: {dice}, Position: {position}")
+        if position == 100:
+            print("You won!!")
 
 snake_and_ladder()
