5fdd7bae62af70cf13d61e02819d12d46d0f77f7
Balamurugan <balamurugan.r050814@gmail.com>
2024-12-07 13:30:53 +0530

    Changed Armstrong Number

--- problems/medium/m15.py
+++ problems/medium/m15.py
@@ -1,38 +1,13 @@
 '''Write a program to check if a number is an Armstrong number (e.g., 153  = 1^3 + 5^3 + 3^3 ) .'''
-a=input("Enter Number:")
-def Arms(a):
-   e=0
-   for i in a:
-      c=int(i)
-      d=c*c*c
-      e+=d
-   return e
 
-<<<<<<< HEAD
-def sum(n):
-   i = count_digits(n)
-   s = 0
-   temp = n
-   while temp > 0:
-      digit = temp%10
-      temp//= 10
-      s += pow(digit,i)
-   return s
+n=input("Enter Number:")
+c=0
+def arm(n):
+   for i in n:
+      c+=int(i)**len(n)
+   if c==int(n):
+      print("It is Armstrong number")
+   else:
+      print("It is not Armstrong Number")
 
-
-num = 1634
-
- 
-s = sum(num)
-
- 
-if s == num:
-   print('Given number is an Armstrong Number')
-=======
-b=Arms(a)
-f=int(a)
-if b==f:
-   print(b,"is an Armstrong Number")
->>>>>>> 246833991f7068b3554261749a57001bddeaec6b
-else:
-   print(b,"is not An Armstrong Number")
+arm(n)
