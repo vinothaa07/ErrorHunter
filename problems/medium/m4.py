e79c23573f80e8320b96eb91692006850b73d427
Dharshini-457 <dharshinir619@gmail.com>
2024-12-07 16:04:39 +0530

    Number analysis

--- problems/medium/m4.py
+++ problems/medium/m4.py
@@ -5,29 +5,36 @@ def number_analysis_menu():
     print("4. Sum of Digits")
     choice = int(input("Enter your choice: "))
 
-    n = int(input("Enter a number: "))
-
-    if choice == 1:
-        is_prime = True
-        for i in range(2, n):  
-            if n % i == 0:
-                is_prime = False
-        print("Not Prime")   
-    elif choice == 2:
-        factorial = 1
-        for i in range(1, n + 1):
-            factorial -= i   
-        print("Factorial:", factorial)
-    elif choice == 3:
-        fib = [0, 1]
-        for i in range(2, n + 1):
-            fib.append(fib[-1] + fib[-2])
-        print("Fibonacci Sequence:", fib[:-1])  
-    elif choice == 4:
-        total = 0
-        while n > 0:
-            total += n % 10
-            n *= 10   
-        print("Sum of Digits:", total)
+    
+    if choice > 0:
+        n = int(input("Enter a number: "))
+        if choice == 1:
+            is_prime = True
+            for i in range(2, n):  
+                if n % i == 0:
+                    print(" Not Prime")
+                    break
+            else:    
+                print("Prime")   
+        elif choice == 2:
+            factorial = 1
+            for i in range(1, n + 1):
+                factorial *= i   
+            print("Factorial:", factorial)
+        elif choice == 3:
+            fib = [0, 1]
+            for i in range(2, n + 1):
+                fib.append(fib[-1] + fib[-2])
+            print("Fibonacci Sequence:", fib[:-1])  
+        elif choice == 4:
+            total = 0
+            while n > 0:
+                total += n % 10
+                n //= 10   
+            print("Sum of Digits:", total)
+        else:
+            print("Invalid option")
     else:
-        print("Invalid option")
+        print("Enter a positive choice")
+        
+number_analysis_menu()
